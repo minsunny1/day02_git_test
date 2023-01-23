3bcb29c754b6bb80a06d896bc8b4e611545d1353
kong3 <122509421+minsunny1@users.noreply.github.com>
2023-01-23 20:36:07 +0900

    Update day01_01_temperature.py

--- day01_01_temperature.py
+++ day01_01_temperature.py
@@ -1,4 +1,4 @@
 # (100°F − 32) × 5/9 = 37.778°C : 주석
 fahrenheit = float(input('화씨 온도 : '))
 celsius = (fahrenheit - 32.0) * (5.0/9.0)
-print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.')
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.')  # f 스트링
